bce13b018d9543826d30a9d81f36ffcbef20d358
pawi1 <p.rutkow@o2.pl>
2024-10-14 11:50:24 +0200

    napisanie zadania 3

--- zadania_1-6/zadanie3.d
+++ zadania_1-6/zadanie3.d
@@ -1,8 +1,32 @@
 import  std.stdio;
 
 // Zadanie 3.
-// Stwórz program który wypisze n liczb bliźniaczych
+// Stwórz program który wypisze n par liczb bliźniaczych
+int isPrime(int a)
+{
+    for(int i = 2;i<a;i++)
+    {
+        if (a % i == 0)
+            return false;
+    }
+    return true;
+}
+int isTwinPrime(int a, int b)
+{
+    return(isPrime(a) && isPrime(b) && (a+2 == b || b+2 == a));
+}    
 void main()
 {
-
+    write("Zadanie 3\nPodaj n: ");
+    int n = 0;
+    readf("%d",&n);
+    writeln("Oto ",n," pary liczb bliżniaczych:");
+    for(int i = 2, j = 0; j<n; i++)
+    {
+        if(isPrime(i) && isPrime(i+2) && isTwinPrime(i,i+2))
+        {    
+            write(i," ",i+2,"\n");
+            j++;
+        }
+    }
 }
